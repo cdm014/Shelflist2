78215af3c7e60fc991a79de953c96e69d443b977
Chester Mealer <cdm014@gmail.com>
2017-10-05 11:52:41 -0500

    updates to the view object

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 
   require_once $settings['views']."index.php";
 
-  $testview = new View("page");
+  $testview = new View("page",$settings);
   $testview->OutputInfo();
+  $html1 = "<P> Can I find the file: ".(file_exists("views/page.tpl")?"Yes":"no")."</p>";
+  //print $html1;
 ?>
--- views/index.php
+++ views/index.php
@@ -9,6 +9,12 @@
       $this->viewName = $name;
       $this->fileString = $this->settings['views'].$this->viewName.".tpl";
       $this->viewData = array();
+      if(file_exists($this->fileString)) {
+        $this->raw = file_get_contents($this->fileString);
+        $this->temp = $this->raw;
+      } else {
+        throw new Exception ("View ".$this->fileString." not found");
+      }
 
     }
 
@@ -16,7 +22,10 @@
       $html = "<h1>View Output</h1>";
       $html .= "<P>View name: ".$this->viewName."</p>";
       $html .= "<p>File Name: ".$this->fileString."</p>";
+      $html .= "<p>Views folder: ".$this->settings['views']."</p>";
       $html .= "<P>Does Template exist: ".(file_exists($this->fileString)?"yes":"no")."</p>";
+      $html .= "<p>Raw Template</p>\r\n";
+      $html .= "<pre>".htmlentities($this->raw)."</pre>\r\n";
       $html .= "<p>View Data</p><pre>".print_r($this->viewData,true)."</pre>";
       print $html;
     }
