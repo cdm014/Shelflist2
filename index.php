192eb31ad6a1091f40111cb2b5935e2436261fa0
cdm014 <cdm014@gmail.com>
2017-10-05 12:02:05 -0500

    remove bug check

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 
   $testview = new View("page",$settings);
   $testview->OutputInfo();
+  $html1 = "<P> Can I find the file: ".(file_exists("views/page.tpl")?"Yes":"no")."</p>";
   //print $html1;
 ?>
